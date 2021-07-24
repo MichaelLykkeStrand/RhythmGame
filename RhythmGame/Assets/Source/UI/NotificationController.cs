995b42a16d1347f44bb7830d1903f85ada68cffc
Michael Strand <32778224+MichaelLykkeStrand@users.noreply.github.com>
2021-07-24 05:45:37 +0200

    Hit and Miss Combo Effects!

--- RhythmGame/Assets/Source/UI/NotificationController.cs
+++ RhythmGame/Assets/Source/UI/NotificationController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreController : MonoBehaviour
+public class NotificationController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ RhythmGame/Assets/Source/UI/ScoreController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ScoreController : MonoBehaviour
+{
+    public GameObject comboGroup;
+    public Text comboText;
+    public AudioClip hitSound;
+    public AudioClip missSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Hit();
+        }
+    }
+
+    public void Hit()
+    {
+        comboGroup.transform.DOPunchScale(new Vector2(-0.2f, -0.2f), 0.1f);
+    }
+
+    public void Miss()
+    {
+        comboGroup.transform.DOShakePosition(0.2f,4f);
+    }
+}
