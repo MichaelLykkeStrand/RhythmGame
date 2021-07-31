bce3dea5de642ffc039eb3c97e4b3129c6d0ff24
Michael Strand <32778224+MichaelLykkeStrand@users.noreply.github.com>
2021-07-31 13:29:52 +0200

    Bird

---
+++ RhythmGame/Assets/BirdMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<Renderer>().isVisible)
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * speed, Space.World);
+        }
+
+    }
+}
